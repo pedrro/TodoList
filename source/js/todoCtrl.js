510d7c4a9faa374e3b52e3e935d3ee4315fe8ea1
Pedro Silva <pedrro2@hotmail.com>
2016-02-07 16:36:34 -0200

    add source of project

---
+++ source/js/todoCtrl.js
@@ -0,0 +1,30 @@
+angular.module('TodoList',[]).controller('TodoCtrl', function($scope) {
+  var bgColor = [
+    {hex: '#086788' },
+    {hex: '#07a0c3' },
+    {hex: '#f0c808' },
+    {hex: '#fff1d0' },
+    {hex: '#dd1c1a' }
+  ]
+  $scope.todos = [
+    {text: 'Learn AngularJS', done:false, color: bgColor[0].hex},
+    {text: 'Build an app', done:false, color: bgColor[0].hex}
+  ];
+
+ $scope.getTotalTodos = function () {
+   return $scope.todos.length;
+ }
+
+ $scope.clearCompleted = function () {
+   $scope.todos = _.filter($scope.todos, function(todo) {
+     return !todo.done;
+   })
+ }
+
+  $scope.addTodo = function () {
+    var randomColor = _.sample(bgColor);
+    $scope.todos.push({ text: $scope.formTodoText, done: false, color:randomColor.hex});
+    $scope.formTodoText = '';
+    $scope.totalTodos += 1;
+  }
+});
