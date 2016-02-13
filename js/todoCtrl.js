1198e00957d006292706621dd74ebc9e2c881f74
Pedro Silva <pedrro2@hotmail.com>
2016-02-13 14:46:29 -0200

    adding some lint and google analytics

--- js/todoCtrl.js
+++ js/todoCtrl.js
@@ -5,23 +5,23 @@ angular.module('TodoList',[]).controller('TodoCtrl', function($scope) {
     {hex: '#f0c808' },
     {hex: '#fff1d0' },
     {hex: '#dd1c1a' }
-  ]
+  ];
   $scope.todos = [];
 
  $scope.getTotalTodos = function () {
    return $scope.todos.length;
- }
+ };
 
  $scope.clearCompleted = function () {
    $scope.todos = _.filter($scope.todos, function(todo) {
      return !todo.done;
-   })
- }
+   });
+ };
 
   $scope.addTodo = function () {
     var randomColor = _.sample(bgColor);
     $scope.todos.push({ text: $scope.formTodoText, done: false, color:randomColor.hex});
     $scope.formTodoText = '';
     $scope.totalTodos += 1;
-  }
+  };
 });
